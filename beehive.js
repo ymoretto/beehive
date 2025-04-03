d67c39d383d51a6c2fd274f1ef648a47803fe472
Yolanda Scudeller <54295639+ymoretto@users.noreply.github.com>
2025-04-03 11:22:52 +0200

    small ux writing update on paragraph #honey

--- beehive.js
+++ beehive.js
@@ -4,26 +4,36 @@ let honey = {
 
 // How JavaScript's Proxy Object Works
 // https://www.freecodecamp.org/news/javascript-proxy-object/
-const honeyProxy = new Proxy(honey, { // this puts obj honey as the target
+const honeyProxy = new Proxy(honey, {
+  // this puts obj honey as the target
   set(target, prop, value) {
-    if(prop === 'nHoney') {
+    if (prop === "nHoney") {
       // Update nHoney count
-      target[prop] = value
+      target[prop] = value;
+
       // Update UI
-      document.getElementById("honey").innerHTML = `You have ${honeyProxy.nHoney} honey cells`;
+      if (honeyProxy.nHoney === 1) {
+        document.getElementById(
+          "honey"
+        ).innerHTML = `You have ${honeyProxy.nHoney} full honey cell`;
+      } else {
+        document.getElementById(
+          "honey"
+        ).innerHTML = `You have ${honeyProxy.nHoney} full honey cells`;
+      }
 
       //check if honey has reached 10
-      if(target.nHoney === 10) {
-        addCollectHoneyBtn()
-      };
+      if (target.nHoney === 10) {
+        addCollectHoneyBtn();
+      }
 
       return true;
     }
-  }
+  },
 });
 
 const addCollectHoneyBtn = () => {
-  let wrapper = document.getElementById('beehive');
+  let wrapper = document.getElementById("beehive");
   let collectBtn = document.createElement("button");
   collectBtn.textContent = "Collect Honey";
   wrapper.appendChild(collectBtn);
